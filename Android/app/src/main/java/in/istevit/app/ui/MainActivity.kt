f172032a5045aa3700029e30d26931a5d6ae0f8a
vandit vasa <vasavandit@gmail.com>
2023-04-03 21:44:50 +0530

    fix: changing image types from PNG to SVG

--- Android/app/src/main/java/in/istevit/app/ui/MainActivity.kt
+++ Android/app/src/main/java/in/istevit/app/ui/MainActivity.kt
@@ -1,23 +1,12 @@
 package `in`.istevit.app.ui
 
-import android.content.ActivityNotFoundException
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import dagger.hilt.android.AndroidEntryPoint
 import `in`.istevit.app.R
 import `in`.istevit.app.databinding.ActivityMainBinding
-import `in`.istevit.app.ui.flagship.FlagshipActivity
-import `in`.istevit.app.ui.projects.ProjectsActivity
-import `in`.istevit.app.ui.resources.ResourcesActivity
-import `in`.istevit.app.util.Constants
-
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
